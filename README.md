ffcf61efa71af6086bbe51d4580b4c91638b545c
smithdanw <dan@innovationsmith.net>
2016-06-29 12:51:49 -0400

    add files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting and Cleaning Data Course Project
+
+run_analysis.R contains 2 functions: process_data & process_set
+process_data takes a working directory where the UCI HAR Dataset exists
+process_set is called from process_data and takes: 
+working directory of HAR Dataset
+sub folder name (e.g. test)
+activity_labels data frame
+features data frame
+
+The values from the subject, X & y files are read and merged into a single data frame and returned. The resulting
+value is also stored in the globalenv as har_totals. The averages for all columns grouped by subject and activity 
+are calculated and stored in by_subject_by_activity. 
+
+To run the code:
+source the script
+har_totals <- process_data(path_to_data)
---
+++ run_analysis.R
@@ -0,0 +1,120 @@
+library(plyr)
+library(dplyr)
+
+process_set <- function (base_folder, sub_folder_name, activity_labels, features){
+  
+  file_name <- paste("X_", sub_folder_name, ".txt", sep = "")
+  test_set <- read.table(paste(base_folder, sub_folder_name, file_name, sep = "/"))
+  
+  file_name <- paste("y_", sub_folder_name, ".txt", sep = "")
+  test_labels <- read.table(paste(base_folder, sub_folder_name, file_name, sep = "/"))
+  
+  file_name <- paste("subject_", sub_folder_name, ".txt", sep = "")
+  subject <- read.table(paste(base_folder, sub_folder_name, file_name, sep = "/"))
+  
+  test_set = test_set[,features$V1]
+  names(test_set) <- features$V2
+  
+  id_vec <- 1:nrow(test_set)
+  
+  test_set$id <- id_vec
+  test_labels$id <- id_vec
+  subject$id <- id_vec
+  
+  names(subject) <- c("subject_id")
+
+  test_set$subject_id <- subject$subject_id
+  activity_vec <- join(x = test_labels, y = activity_labels, by = "V1", type="left" )
+  names(activity_vec) <- c("V1", "id", "activity")
+  test_set$activity_name <- activity_vec$activity
+  test_set$group <- rep(sub_folder_name, nrow(test_set))
+  
+  test_set
+}
+
+process_data <- function(base_folder)
+{
+  activity_labels <- read.table(paste(base_folder, "activity_labels.txt", sep = "/"), stringsAsFactors = F)
+  features <- read.table(paste(base_folder, "features.txt", sep = "/"))
+  features <- mutate(features, V2 = tolower(V2))
+  matches <- data.frame(grep("^(t|angle).*(mean|std)", features$V2))
+  names(matches) <- ("grep_result")
+  matches <- mutate(matches, keep = T)
+  features <- merge(x = features, by.x = "V1", y = matches, by.y = "grep_result", all.x =  T)
+  features <- filter(features, !is.na(keep) & keep == T)
+  features <- mutate(features, V2 = gsub("[(]|[)]", "", V2))
+  features <- mutate(features, V2 = gsub("[-]", "_", V2))
+  features <- mutate(features, V2 = sub("tbody", "body_", V2))
+  features <- mutate(features, V2 = sub("tgravity", "gravity_", V2))
+  features <- mutate(features, V2 = sub("anglebody", "angle_body", V2))
+  features <- mutate(features, V2 = sub("gravitymean", "gravity_mean", V2))
+  features <- mutate(features, V2 = sub("accjer", "acc_jerk", V2))
+  features <- mutate(features, V2 = sub("gyrojerk", "gyro_jerk", V2))
+  features <- mutate(features, V2 = sub("accmag", "acc_mag", V2))
+  features <- mutate(features, V2 = sub("accjerkmag", "acc_jerk_mag", V2))
+  features <- mutate(features, V2 = sub("gyromag", "gyro_mag", V2))
+  features <- mutate(features, V2 = sub("jerkmag", "jerk_mag", V2))
+  features <- mutate(features, V2 = sub("anglex,", "angle_x_", V2))
+  features <- mutate(features, V2 = sub("angley,", "angle_y_", V2))
+  features <- mutate(features, V2 = sub("anglez,", "angle_z_", V2))
+  features <- mutate(features, V2 = sub("accmean,", "acc_mean_", V2))
+  features <- mutate(features, V2 = sub("jerkmean,", "jerk_mean_", V2))
+  
+  totals = 1
+  
+  for(directory in list.dirs(base_folder, recursive = FALSE)){
+    if(directory != base_folder){
+      folder_name <- sub(".*[/]", "", directory)
+      temp <- process_set(base_folder, folder_name, activity_labels, features)
+
+      #merge the indivual trials together
+      if(is.numeric(totals)) {
+        totals <- temp 
+      } else
+      {
+        totals <- rbind.fill(totals, temp)
+      }
+    }
+  }
+ 
+  totals$id = NULL
+  
+ created_columns <- c("subject_id", "activity_name", "group")                        
+ 
+ my_names <- names(totals)
+ splits <- split(totals, list(totals$subject_id, totals$activity_name))
+ 
+ num_splits <- length(splits)
+ split_names <- names(splits)
+ 
+  output <- apply(data.frame(my_names), 1, function(x){
+    if(!any(x == created_columns))
+    {
+      values <- lapply(splits, function(element){
+        df1 <- data.frame(element)
+        names(df1) <- my_names
+        mean(df1[,x])
+      })
+  
+      names(values) <- NULL
+      vec <- unlist(values)
+      vec
+    } else if (x == "subject_id"){
+       activity_name <- gsub("[.].*", "", split_names)
+    } else if (x == "activity_name"){
+       activity_name <- gsub(".*[.]", "", split_names)
+    } else{
+      rep(x = x, num_splits)
+    }
+  })
+ 
+  by_subject_by_activity <- data.frame(output)
+  names(by_subject_by_activity) <- my_names
+  by_subject_by_activity$group <- NULL
+  
+  env <- globalenv()
+  env$har_totals <- totals
+  env$by_subject_by_activity <- by_subject_by_activity
+  
+  totals
+}
